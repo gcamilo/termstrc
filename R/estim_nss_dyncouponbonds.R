06f6a5532215bb4e1d150a4c8d35a53fe8dc7516
Gustavo Camilo <gcamilo@gmail.com>
2018-11-10 12:04:57 -0500

    char date

--- R/estim_nss_dyncouponbonds.R
+++ R/estim_nss_dyncouponbonds.R
@@ -22,7 +22,7 @@ estim_nss.dyncouponbonds <- function(dataset, group, matrange="all",method="ns",
     ## static estimation
     bonddata <- dataset[[i]]
     res[[i]] <- estim_nss(bonddata, group, matrange, method=method, startparam=b, lambda=lambda,tauconstr,constrOptimOptions)
-    res[[i]]["ddate"] <- bonddata$RD$TODAY
+    res[[i]]["ddate"] <- as.character(bonddata$RD$TODAY)
   }
   class(res) <- "dyntermstrc_nss"
 
